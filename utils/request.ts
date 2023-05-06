b3101126e476660e1616bc9f00ab94b5b8543600
Ting Hu <yutinghu813@gmail.com>
2023-05-06 15:18:14 +0800

    測試commit

--- utils/request.ts
+++ utils/request.ts
@@ -1,5 +1,4 @@
-const API_KEY = '6d880c955f512e1dbf62e6cfca4b9fa0';
-// process.env.NEXT_PUBLIC_API_KEY;
+const API_KEY =  process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const requests = {
